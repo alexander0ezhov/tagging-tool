866079ede4680248edd8ea35c5a415765cf30f1c
Alexander Ezhov <aezhov@inno.tech>
2022-05-13 10:37:24 +0300

    react-scripts -> webpack

--- src/index.tsx
+++ src/index.tsx
@@ -1,14 +1,14 @@
 import React from "react";
 import { createRoot } from 'react-dom/client'
 
-import TaggingCanvas from 'tagging-canvas'
+// import TaggingCanvas from 'tagging-canvas'
 import s from './index.module.scss'
 
 const cvs = (document.getElementById('canvas') as HTMLCanvasElement);
 const root = createRoot((document.getElementById('root') as HTMLCanvasElement));
 
-const tc = new TaggingCanvas({node: cvs})
+// const tc = new TaggingCanvas({node: cvs})
 
-console.log(tc)
+// console.log(tc)
 
 root.render(<div className={s.qwe}>qwe</div>)
