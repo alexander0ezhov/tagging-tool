aa719efc4a0cd52a76b5b2ea0b0c1fcfd172afb7
alexander0ezhov <alexander0ezhov@hotmail.com>
2022-05-28 02:30:18 +0300

    tagging-canvas package

--- src/index.tsx
+++ src/index.tsx
@@ -3,4 +3,8 @@ import ReactDom from 'react-dom'
 
 import s from './index.module.scss'
 
+import TaggingCanvas from "tagging-canvas";
+
+console.log(TaggingCanvas)
+
 ReactDom.render(<div className={s.qwe}>qwe</div>, document.getElementById('root'))
